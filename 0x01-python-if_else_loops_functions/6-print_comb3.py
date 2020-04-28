a34bc4a897692596fe03698e042324eade5bd6a3
Leidysalda <leidysalda1@gmail.com>
2020-04-28 20:26:54 +0000

    Fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for n in range(100):
     p = n % 10
 
     if o < p and o != p and n != 89:
-        print("{:02d}".format(n), end=',')
+        print("{:02d}".format(n), end=', ')
     elif n == 89:
         print("{:02d}".format(n))
