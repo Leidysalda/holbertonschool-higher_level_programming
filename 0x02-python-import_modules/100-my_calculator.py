5e4c8bc62fe505a189650d783153f946bc62e99d
Leidysalda <leidysalda1@gmail.com>
2020-05-01 02:52:54 +0000

    Fix init

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,8 @@
 from sys import argv
 from calculator_1 import add, sub, mul, div
 
-try:
+
+if __name__ == "__main__":
     if len(argv) == 4:
         a = (int(argv[1]))
         b = (int(argv[3]))
@@ -18,8 +19,5 @@ try:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
     else:
-        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
-except Exception as e:
-    pass
