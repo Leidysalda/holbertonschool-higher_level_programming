851b52ce348c054ccc854a7586d9b883ef1aad3b
Leidysalda <leidysalda1@gmail.com>
2020-05-01 00:53:12 +0000

    [Feature]: Add file

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+try:
+    if len(argv) == 4:
+        a = (int(argv[1]))
+        b = (int(argv[3]))
+        if argv[2] == '+':
+            print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
+        elif argv[2] == '-':
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a,b)))
+        elif argv[2] == '*':
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a,b)))
+        elif argv[2] == '/':
+            print("{:d} / {:d} = {:d}".format(a, b, div(a,b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+    else:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+
+except Exception as e:
+    print(e)
