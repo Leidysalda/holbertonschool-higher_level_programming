2b5c3ee6b4b289edd76de8debee2885f79b1d249
Leidysalda <leidysalda1@gmail.com>
2020-05-06 02:30:30 +0000

    [Fix] pep8

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
+
 def add_tuple(tuple_a=(), tuple_b=()):
-   tuple_c = ()
-   tuple_d = (0, 0)
+    tuple_c = ()
+    tuple_d = (0, 0)
 
-   if len(tuple_a) < 2:
-      tuple_a = tuple_a + tuple_d
-   if len(tuple_b) < 2:
-      tuple_b = tuple_b + tuple_d
+    if len(tuple_a) < 2:
+        tuple_a = tuple_a + tuple_d
+    if len(tuple_b) < 2:
+        tuple_b = tuple_b + tuple_d
 
-   tuple_c = tuple(i + j for i, j in zip(tuple_a[0:2], tuple_b[0:2]))
-   return (tuple_c)
+    tuple_c = tuple(i + j for i, j in zip(tuple_a[0:2], tuple_b[0:2]))
+    return (tuple_c)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
+
 def max_integer(my_list=[]):
-   if my_list == '':
-      return None
-   list_ord = sorted(my_list)
-   return (list_ord[-1])
+    if my_list == '':
+        return None
+        list_ord = sorted(my_list)
+        return (list_ord[-1])
