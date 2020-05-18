09d8da4ae137add4663048a165c95b021f453948
Leidysalda <leidysalda1@gmail.com>
2020-05-18 22:45:27 +0000

    Fix stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print('{:d}'.format(value))
         return True
     except Exception as err:
-         sys.stderr.write('Exception: {}\n'.format(err))
-         return False
+        sys.stderr.write('Exception: {}\n'.format(err))
+        return False
