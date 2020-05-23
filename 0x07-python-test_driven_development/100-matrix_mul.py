3b698422cf243a13188b28ccfa9ad7d8454db406
Leidysalda <leidysalda1@gmail.com>
2020-05-23 23:37:15 +0000

    Add file

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -8,9 +8,18 @@ def matrix_mul(m_a, m_b):
     """
     function that multiplies 2 matrices
     """
-    filas_a = len(m_a)
-    col_a = len(m_a[0])
-    filas_b = len(m_b)
-    col_b = len(m_b[0])
+    if type(m_a) is not list:
+        raise TypeError('m_a must be a list')
+    elif type(m_b) is not list:
+        raise TypeError('m_b must be a list')
+    elif type(m_a) is None:
+        raise ValueError('m_a can\'t be empty')
+    elif type(m_b) is None:
+        raise ValueError('m_b can\'t be empty')
 
-    return(mulMatrices(m_a, m_b))
+    m_c = [[0 for row in range(len(m_a))] for col in range(len(m_b[0]))]
+    for i in range(len(m_a)):
+        for j in range(len(m_b[0])):
+                       for k in range(len(m_b)):
+                           m_c[i][j] += m_a[i][k] * m_b[k][j]
+    return(m_c)
