71f9796cba623c09a822b0fcabb631922fe860a4
Leidysalda <leidysalda1@gmail.com>
2020-05-27 01:49:01 +0000

    Fix ffile

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(n=[0]):
-    n[0] += 1; return('Holberton, ' * n[0])[0:len('Holberton, ' * n[0]) - 2]
-    for i in range(n): print(magic_string())
+def magic_string(n=[]):
+    n += ['Holberton']
+    return ', '.join(n)
