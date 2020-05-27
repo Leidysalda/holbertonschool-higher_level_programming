12df12354885e330a8a5991aa5b6765c25c0727a
Leidysalda <leidysalda1@gmail.com>
2020-05-27 00:52:38 +0000

    [Fix]file

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""Magic string"""
-
-
 def magic_string(n=[0]):
-    """Magic string"""
     n[0] += 1; return('Holberton, ' * n[0])[0:len('Holberton, ' * n[0]) - 2]
     for i in range(n): print(magic_string())
