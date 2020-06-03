f8a1581d63fdf25239adc9c22852ef0d190f8625
Leidysalda <leidysalda1@gmail.com>
2020-06-03 03:00:40 +0000

    Fix file

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,28 +1,6 @@
 #!/usr/bin/python3
 """Square #1"""
-
-
-class BaseGeometry:
-    """Class Base"""
-
-    def __dir__(self):
-        """function __dir__ """
-        obj = BaseGeometry
-        return dir(obj)
-
-    def area(self):
-        """Method area"""
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        """Method validator"""
-        self.name = name
-        self.value = value
-
-        if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(self.name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(self.name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
