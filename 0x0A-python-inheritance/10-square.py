2f505a5050092cd687ad693e0924d582dbd098f7
Leidysalda <leidysalda1@gmail.com>
2020-06-03 03:10:45 +0000

    Fix

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,14 +7,14 @@ class Rectangle(BaseGeometry):
     """class son rectangle"""
     def __init__(self, width, height):
         """Constructor"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
 
     def area(self):
         """"Methodd area"""
-        return (self.__width * self.__height)
+        return self.__width * self.__height
 
     def __str__(self):
         """Method __str__"""
@@ -26,10 +26,8 @@ class Square(Rectangle):
     def __init__(self, size):
         """Constructor"""
         self.__size = size
-        self.integer_validator("size", size)
-        super().__init__(self.__size, self.__size)
 
-    def are(self):
+    def area(self):
         """area"""
         return self.__size * self.__size
 
