db0587532bb828932555dcda3fdc1848533f617a
Leidysalda <leidysalda1@gmail.com>
2020-06-04 02:24:12 +0000

    Add file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ def read_file(filename=""):
     """
     function that reads a text file (UTF8) and prints it to stdout
     """
-    with open(filename) as file:
+    with open(filename, 'r', encoding='utf8') as file:
         a = file.read()
         print(a, end='')
