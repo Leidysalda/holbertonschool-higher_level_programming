826f5c8a23ed459267ed25da1f78942be22d6994
Leidysalda <leidysalda1@gmail.com>
2020-06-04 17:00:08 +0000

    [Feature]Add file

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+
+class Student:
+    """class student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if type(attrs) is not list:
+            return self.__dict__
+        dict = {}
+
+        for i in attrs:
+            v = getattr(self, i, None)
+            if v is not None:
+                dict[i] = v
+        return dict
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        if not json:
+            return
+        self.__dic__ = json
