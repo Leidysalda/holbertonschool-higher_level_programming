5d48b5f2b114b0218873c945797eaa358dfc94cd
Leidysalda <leidysalda1@gmail.com>
2020-06-04 02:31:01 +0000

    Fix head

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Save Object to a file"""
 import json
 
--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Create object from aa JSON file"""
 import json
 
