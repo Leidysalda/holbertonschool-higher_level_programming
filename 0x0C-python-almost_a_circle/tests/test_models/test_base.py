c5d446e9a3c0d02f44adbaa762fb6b77425f2d88
Leidysalda <leidysalda1@gmail.com>
2020-06-14 20:23:10 +0000

    Fix test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,6 +4,8 @@ Test
 """
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class test_Base(unittest.TestCase):
@@ -14,3 +16,9 @@ class test_Base(unittest.TestCase):
         """1"""
         b1 = Base()
         self.assertEqual(b1.id, 1)
+        b2 = Base(8)
+        self.assertEqual(b2.id, 8)
+        b3 = Base('a')
+        self.assertEqual(b3.id, 'a')
+        b4 = Base(89)
+        self.assertEqual(b4.id, 89)
