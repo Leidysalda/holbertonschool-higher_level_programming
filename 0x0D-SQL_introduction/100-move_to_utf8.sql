795b66e7cadec4fa1b8391698ed7f2fedbe8e3f7
Leidysalda <leidysalda1@gmail.com>
2020-07-14 03:30:15 +0000

    [Fix] Add change

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,6 @@
 -- script that converts hbtn_0c_0
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
-ALTER TABLE first_table COLLATE utf8_general_ci;
-SELECT name CONVERT(CAST(name) USING utf8) FROM first_table;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE = utf8_general_ci;
+
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE = utf8_general_ci;
+
+ALTER TABLE first_table CHANGE name name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8_general_ci;
