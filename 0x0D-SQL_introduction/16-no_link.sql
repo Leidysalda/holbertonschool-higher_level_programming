40ec994b3677121c08b84cc15ca534723bc1870d
Leidysalda <leidysalda1@gmail.com>
2020-07-14 03:22:20 +0000

    [Fix] The diferent IS NOT

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- script that lists all records of the table second_table of the database.
-SELECT score, name FROM second_table WHERE name != "" GROUP BY score score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
