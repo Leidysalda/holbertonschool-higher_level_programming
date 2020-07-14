e88dbdd7e989456f46605868caefaf09be9d802c
Leidysalda <leidysalda1@gmail.com>
2020-07-14 03:42:38 +0000

    [Fix] Add change

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- script that displays the average temperature (Fahrenheit).
-SELECT state, MAX (value) AS max_temp FROM temperatures
+SELECT state, MAX(value) AS max_temp FROM temperatures
 GROUP BY state ORDER BY state ASC;
