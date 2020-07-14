25b1309475967394e5e0d0c7504fc8b59b217e2a
Leidysalda <leidysalda1@gmail.com>
2020-07-14 03:39:35 +0000

    [Fix] Add change

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,3 @@
 -- script that displays the average temperature (Fahrenheit).
-SELECT AVG(value) AS avg_temp FROM temperatures GROUP BY state ORDER BY value DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+GROUP BY city ORDER BY avg_temp DESC;
