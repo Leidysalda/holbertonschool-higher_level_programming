4a95c3970ac2e3816aecd9a6b2fb9a78811e1b18
Leidysalda <leidysalda1@gmail.com>
2020-07-14 03:32:57 +0000

    [Fix] Add change

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,6 @@
 -- script that converts hbtn_0c_0
+USE hbtn_0c_0
+
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE = utf8_general_ci;
 
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE = utf8_general_ci;
