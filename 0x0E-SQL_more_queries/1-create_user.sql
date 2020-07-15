de3af581288f69d1b3a4f50978886d6c483aadcd
Leidysalda <leidysalda1@gmail.com>
2020-07-15 04:25:47 +0000

    [Feature]Add files

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- script that lists all privileges of the MySQL users
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,3 @@
+# 0x0E. SQL - More queries
+
+![](https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/66988091.jpg)
