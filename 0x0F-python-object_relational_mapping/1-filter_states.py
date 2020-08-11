550a1c5f2a47a63f4ee93da20d29d3078ac0cfe4
leidysalda <leidysalda1@gmail.com>
2020-08-11 02:40:22 +0000

    [Fix] pep8 style

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- script that lists all states from the database hbtn_0e_0_usa
+script that lists all states with a name starting with N (upper N)
 """
 import MySQLdb
 import sys
