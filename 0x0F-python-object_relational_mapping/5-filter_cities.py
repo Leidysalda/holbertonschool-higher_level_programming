737248791ff6a4e6777be025c8cd73d231dc3306
leidysalda <leidysalda1@gmail.com>
2020-08-11 16:56:52 +0000

    [Fix]files

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,15 +13,17 @@ if __name__ == '__main__':
                          db=sys.argv[3], charset="utf8")
 
     cur = db.cursor()
-    state = (sys.argv[4])
+    state = sys.argv[4]
     cur.execute("SELECT cities.name FROM cities JOIN states\
-    ON cities.state_id = states.id WHERE states.name = '%state'")
+    ON cities.state_id = states.id WHERE states.name = %(state)s", {'state' : state})
 
     rows = cur.fetchall()
+
     for i in range(len(rows)):
-        print(rows[0], end="")
+        print(rows[i][0], end="")
         if i != len(rows) - 1:
             print(", ", end="")
     print("")
+    
     cur.close()
     db.close()
