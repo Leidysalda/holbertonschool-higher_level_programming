967a0706f2748d1f791b1742f35742e0d28698b9
leidysalda <leidysalda1@gmail.com>
2020-08-19 03:58:28 +0000

    [Feature]Add files

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Bash script that takes in a URL, sends a POST request to the passed URL
+curl -s -X POST "$1" -d "email=emailhr@holbertonschool.com&subject=I will always be here for PLD"
