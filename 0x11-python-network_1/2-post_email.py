908f37faab656ef009f810663b93b8dadbbe0819
leidysalda <leidysalda1@gmail.com>
2020-08-24 23:53:02 +0000

    [Feature]Add file 2-

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,13 +10,13 @@ import urllib.parse
 
 def status():
     """URL"""
-    value = {'email' : sys.argv[2]}
+    value = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(value)
     data = data.encode('utf-8')
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
         page = response.read()
-    print('Your email is: {}'.format(sys.argv[2]))
+    print(page.decode('utf-8'))
 
 
 if __name__ == '__main__':
