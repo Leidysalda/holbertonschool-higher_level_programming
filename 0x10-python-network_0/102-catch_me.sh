1052867f030d423149054ed9b4ca60f117ac0cfa
leidysalda <leidysalda1@gmail.com>
2020-08-24 19:40:48 +0000

    [Feature]Add file

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request to 0.0.0.0:5000/catch_me
-curl -sL -X PUT "$1" -d "You got me!" -H "origin: HolbertonSchool"
+curl -sL -X PUT http://0.0.0.0:5000/catch_me -d "user_id=98" -H "origin: HolbertonSchool"
