7507f51349bc253591cd1f5437222fa8580a48d5
leidysalda <leidysalda1@gmail.com>
2020-09-01 16:37:58 +0000

    [Fix] file 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == '__main__':
 
     url = "https://api.github.com/repos/{}/{}/commits"
-    .format(sys.argv[1], sys.argv[2exi])
+    .format(sys.argv[1], sys.argv[2])
 
     res = requests.get(url)
 
