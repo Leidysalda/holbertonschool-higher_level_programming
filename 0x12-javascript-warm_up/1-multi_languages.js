4609b641b6d55431d734588bd07baac6f85262e7
leidysalda <leidysalda1@gmail.com>
2020-09-15 20:11:25 +0000

    [Fix]Type var 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that prints 3 lines
 
-var lineC = 'C is fun \nPython is cool \nJavascript is amazing';
+const lineC = 'C is fun \nPython is cool \nJavascript is amazing';
 
 function logName () {
   console.log(lineC);
