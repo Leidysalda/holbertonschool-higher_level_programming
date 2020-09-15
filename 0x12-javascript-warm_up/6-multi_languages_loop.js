600194ac450d611ca9d034b0a7e4f247450d1348
leidysalda <leidysalda1@gmail.com>
2020-09-15 23:55:44 +0000

    [Feature] Add 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/** script that prints My number: <first argument converted in integer>
+    if the first argument can be converted to an integer.
+*/
+
+const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+lang.forEach(funcArr);
+
+function funcArr (item) {
+  console.log(item);
+}
