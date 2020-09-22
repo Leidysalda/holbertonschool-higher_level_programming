bc6ae374ac9fa7de34d04e23bd16f738c5cf74b8
leidysalda <leidysalda1@gmail.com>
2020-09-22 05:18:20 +0000

    [Feature]Add file 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,5 +2,8 @@
 // function that returns the reversed version of a list
 
 exports.esrever = function (list) {
-  return list.reverse();
+  for (let i = 0; i < list.length / 2; i++) {
+    [list[i], list[list.length - i - 1]] = [list[list.length - i - 1], list[i]];
+  }
+  return list;
 };
