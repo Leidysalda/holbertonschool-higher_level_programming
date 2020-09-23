46c04cdc7b76afbd79c650ff258f85aef7d2d5fe
leidysalda <leidysalda1@gmail.com>
2020-09-23 05:37:10 +0000

    [Fix] response status file 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // script that display the status code of a GET request.
 
-const url = process.argv[2];
 const request = require('request');
+const url = process.argv[2];
 
-request(url, function (response) {
+request.get(url).on('response', function (response) {
   console.log('code: %d', response.statusCode);
 });
